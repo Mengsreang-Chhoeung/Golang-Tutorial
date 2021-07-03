145d73cfb3dd4d5722cfd5b6827d840789ba5c7d
chhoeung-mengsreang <chheoungmengsreang789@gmail.com>
2021-07-03 11:46:10 +0700

    add appFour

---
+++ src/appFour.go
@@ -0,0 +1,89 @@
+package main
+import "fmt"
+
+func main() {
+	var number int = 100
+	var floatingNumber float32 = 123.45
+	var text string = "Hello World"
+	var isMarried bool = true
+
+	fmt.Println("1------------------------")
+	fmt.Println("Number: ", number)
+	fmt.Println("Floating Number: ", floatingNumber)
+	fmt.Println("Text: ", text)
+	fmt.Println("Married: ", isMarried)
+	fmt.Println("-------------------------")
+
+	fmt.Print("\n")
+
+	// change value in variable
+	number = 200
+	floatingNumber = 789.65
+	text = "Hello Gooolang"
+	isMarried = false
+
+	fmt.Println("2------------------------")
+	fmt.Println("Number: ", number)
+	fmt.Println("Floating Number: ", floatingNumber)
+	fmt.Println("Text: ", text)
+	fmt.Println("Married: ", isMarried)
+	fmt.Println("-------------------------")
+
+	fmt.Print("\n")
+
+	var numberTwo = 1000
+	var floatingNumberTwo = 123.455
+	var textTwo = "Hello World Go"
+	var isMarriedTwo = false
+
+	fmt.Println("3------------------------")
+	fmt.Println("Number: ", numberTwo)
+	fmt.Println("Floating Number: ", floatingNumberTwo)
+	fmt.Println("Text: ", textTwo)
+	fmt.Println("Married: ", isMarriedTwo)
+	fmt.Println("-------------------------")
+
+	fmt.Print("\n")
+
+	// change value in variable
+	numberTwo = 200
+	floatingNumberTwo = 789.65
+	textTwo = "Hello Gooolang"
+	isMarriedTwo = true
+
+	fmt.Println("4------------------------")
+	fmt.Println("Number: ", numberTwo)
+	fmt.Println("Floating Number: ", floatingNumberTwo)
+	fmt.Println("Text: ", textTwo)
+	fmt.Println("Married: ", isMarriedTwo)
+	fmt.Println("-------------------------")
+
+	fmt.Print("\n")
+
+	numberThree := 1000
+	floatingNumberThree := 123.455
+	textThree := "Hello World Go"
+	isMarriedThree := false
+
+	fmt.Println("5------------------------")
+	fmt.Println("Number: ", numberThree)
+	fmt.Println("Floating Number: ", floatingNumberThree)
+	fmt.Println("Text: ", textThree)
+	fmt.Println("Married: ", isMarriedThree)
+	fmt.Println("-------------------------")
+
+	fmt.Print("\n")
+
+	// change value in variable
+	numberThree = 200
+	floatingNumberThree = 789.65
+	textThree = "Hello Gooolang"
+	isMarriedThree = true
+
+	fmt.Println("6------------------------")
+	fmt.Println("Number: ", numberThree)
+	fmt.Println("Floating Number: ", floatingNumberThree)
+	fmt.Println("Text: ", textThree)
+	fmt.Println("Married: ", isMarriedThree)
+	fmt.Println("-------------------------")
+}
